e37c61ff849e3716c937d41900b2fe873c05fa26
marjakorander <marja.korander@gmail.com>
2019-08-06 21:20:02 +0300

    lopullinen kolmonen

--- models/person.js
+++ models/person.js
@@ -4,11 +4,13 @@ const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true })
+  // eslint-disable-next-line no-unused-vars
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -21,21 +23,21 @@ const personSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
-  },  
+  },
   number: {
     required: true,
     type: String
-  }  
+  }
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
